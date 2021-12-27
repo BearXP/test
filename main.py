1172dcece6e0cabb8b2d4d137cd44cdd6f9bc756
Mark <president.evans@gmail.com>
2021-12-27 12:40:47 +1100

    Moving from EEL to Flask  - Drag & Drop javascript working

--- main.py
+++ main.py
@@ -1,11 +1,27 @@
-import eel
+from flask import Flask, render_template, request
+from werkzeug.utils import secure_filename
+from os import path
+app = Flask(__name__)
 
-eel.init('web')
-
-
-@eel.expose
-def processFile(a, b):
-    print(a, b, a + b)
-
-
-eel.start('main.html')
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
+	
+@app.route('/uploader', methods = ['GET', 'POST'])
+def uploader_file():
+    print("Uploader running!")
+    if request.method == 'POST':
+        print("****************")
+        print(request)
+        print("****************")
+        for key in request.files:
+            f = request.files[key]# ['file']
+            print(request)
+            print("****************")
+            output_filename = secure_filename(f.filename)
+            output_filepath = path.join("Uploads", output_filename)
+            f.save(output_filepath)
+            return 'file uploaded successfully'
+		
+if __name__ == '__main__':
+   app.run(debug = False)
